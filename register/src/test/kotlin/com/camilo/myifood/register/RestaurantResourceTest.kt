1c0186974ed3a0573cc7d7a90083a6a002bc23d6
Luis Camilo <lhenriquegomescamilo@hotmail.com>
2020-10-11 21:14:55 -0300

    Creating the first test of integration to restaurant resource.

--- register/src/test/kotlin/com/camilo/myifood/register/RestaurantResourceTest.kt
+++ register/src/test/kotlin/com/camilo/myifood/register/RestaurantResourceTest.kt
@@ -1,24 +1,33 @@
 package com.camilo.myifood.register
 
+import com.github.database.rider.cdi.api.DBRider
+import com.github.database.rider.core.api.configuration.DBUnit
+import com.github.database.rider.core.api.configuration.Orthography
+import com.github.database.rider.core.api.dataset.DataSet
 import io.quarkus.test.common.QuarkusTestResource
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import org.apache.http.HttpStatus
 import org.approvaltests.Approvals
-import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
 
+@DBRider
+@DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
 @QuarkusTest
 @QuarkusTestResource(RegisterTestLifecycleManager::class)
 class RestaurantResourceTest {
 
     @Test
+    @DataSet(value = ["restaurants.yml"])
     fun `should return the list of restaurants`() {
-        given()
+        val result = given()
             .`when`().get("/restaurants")
             .then()
             .statusCode(HttpStatus.SC_OK)
             .extract().asString()
+        Approvals.verifyJson(result)
     }
+
+
 
 }
